3f4bf16ab5f440ea31a3a5377bfc77f3dd0c5cd0
Timvankooten <151274@csgaugustinus.nl>
2019-12-17 13:19:22 +0000

    DG

--- JS/H1/H1O30.js
+++ JS/H1/H1O30.js
@@ -8,7 +8,13 @@ function setup() {
 }
 
 function draw() {
+    for (var rij = 0;rij < 450;rij += 50) {
     for (var kolom = 0;kolom < 450;kolom += 50) {
-      rect(kolom,rij,50,50);
+
+        if (rij == 150 && kolom == 300){ fill('orange')}
+
+        else{ fill('white')}
+      rect(kolom,rij,50,50);   
+    }
     }
 }
